8a5266e09e7efb19c07388b711044f99aa47b260
younusahmad <131500705+younusahmad@users.noreply.github.com>
2025-04-28 02:29:10 +0300

    Delete admin directory

--- admin/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$con = mysqli_connect('localhost','root','','shop_db');
-?>
--- admin/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include('config.php');
-$ID = $_GET['id'];
-mysqli_query($con, "DELETE FROM products WHERE id=$ID");
-header('location: products.php')
-
-?>
